1457a0044feaca5412fe59fe8ca6e1d125eac027
navadeep1012 <navadeep1012@gmail.com>
2023-03-12 17:19:35 +0530

    Dom QuerySelectorAll task

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 // EXAMINE THE DOCUMENT OBJECT //
-console.log(document);
-console.log(document.URL);
-console.log(document.title);
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[10]);
-//document.all[10].textContent = 'Hello';
+// console.log(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// //document.all[10].textContent = 'Hello';
 
-console.log(document.forms);
-console.log(document.links);
+// console.log(document.forms);
+// console.log(document.links);
 
-console.log(document.images);
+// console.log(document.images);
 
 // GET ELEMENT BY ID //
 
-console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-console.log(headerTitle);
-headerTitle.textContent = 'Hello';
-headerTitle.innerText = 'GoodBye';
-console.log(headerTitle.innerText);
-headerTitle.innerHTML = '<h3>Hello</h3>'
+// console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'GoodBye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>'
 
 
 // 
@@ -92,16 +92,16 @@ headerTitle.innerHTML = '<h3>Hello</h3>'
 
 //get elements by tag name //
 
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent = 'hello 2';
-li[1].style.fontWeight = 'bold';
-li[1].style.backgroundColor = 'yellow';
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
 
-for(var i=0;i<li.length;i++){
-    li[i].style.backgroundColor = '#f4f4f4'
-}
+// for(var i=0;i<li.length;i++){
+//     li[i].style.backgroundColor = '#f4f4f4'
+// }
 
 // // QUERY SELECTOR
 //  var header = document.querySelector('#main-header');
@@ -124,5 +124,33 @@ for(var i=0;i<li.length;i++){
  
 // var input = document.querySelector('input');
 // input.value = 'Hello World'
+
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = "green";
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility = 'hidden';
+
+// QUERY SELECTOR ALL //
+ 
+// var titles = document.querySelectorAll('.title');
+// console.log(titles);
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+
+ for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'green';
+}
+
+var secondItem = document.querySelectorAll('li:nth-child(2)');
+
+for(var i=0;i<secondItem.length;i++){
+    secondItem[i].style.color = 'green';
+}
+
+
+
 
 
