50c08db6c818bb06143d2f2d4dc0c34c17185980
navadeep1012 <navadeep1012@gmail.com>
2023-03-13 14:32:26 +0530

    Creating nodes and modifying dom task

--- main.js
+++ main.js
@@ -126,29 +126,129 @@
 // input.value = 'Hello World'
 
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = "green";
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = "green";
 
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.visibility = 'hidden';
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility = 'hidden';
 
 // QUERY SELECTOR ALL //
  
 // var titles = document.querySelectorAll('.title');
 // console.log(titles);
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
 
 
- for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor = 'green';
-}
+//  for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor = 'green';
+// }
 
-var secondItem = document.querySelectorAll('li:nth-child(2)');
 
-for(var i=0;i<secondItem.length;i++){
-    secondItem[i].style.color = 'green';
-}
+
+// var secondItem = document.querySelectorAll('li:nth-child(2)');
+
+// for(var i=0;i<secondItem.length;i++){
+//     secondItem[i].style.color = 'green';
+// }
+
+
+// TRAVERSING THE DOM //
+
+var itemList = document.querySelectorAll('#items');
+// PARENT NODE //
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// PARENT ELEMENT //
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// CHILD NODES //
+// 
+
+// CHILDRENS NODE //
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// // FIRST CHILD //
+//  console.log(itemList.firstElementChild);
+
+//  // FIRST ELEMENT CHILD //
+//  console.log(itemList.firstElementChild);
+//  itemList.firstElementChild.textContent = "Hello";
+
+// LAST CHILD //
+//  console.log(itemList.lastChild);
+
+ // LAST ELEMENT CHILD //
+//  console.log(itemList.lastElementChild);
+//  itemList.lastElementChild.textContent = "Hello";
+
+// NextSibling //
+// console.log(itemList.nextSibling);
+
+// // NextElementSibling //
+// console.log(itemList.nextElementSibling);
+
+// previous sibling
+// console.log(itemList.previousSibling);
+
+// previous element sibling 
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+
+// CreateElement
+
+//Create a div
+
+var newDiv = document.createElement('div');
+
+//Add class
+newDiv.className = 'hello';
+
+//Add id
+newDiv.id = 'hello1';
+
+//Add attr
+newDiv.setAttribute('little', 'Hello Div');
+
+
+// create text node
+var newDivText = document.createTextNode('HEllo World');
+
+
+//Add text to div 
+newDiv.appendChild(newDivText);
+
+
+
+
+
+ console.log(container);
+
+
+
+//  console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+
+// cont.insertBefore(newDiv,item);
+
+// Insert helloworld before item1
+var container = document.querySelector('.list-group');
+var cont = document.querySelector('.list-group-item');
+container.insertBefore(newDiv,cont);
+
+
+// Insert helloworld before itemlister
+var container1 = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container1.insertBefore(newDiv,h1);
 
 
 
